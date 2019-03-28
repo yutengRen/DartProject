cc1c0cbd0ad83ada79b651c8c51e81661e617903
unknown <Leeningfeng@163.com>
2019-03-28 16:43:47 +0800

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ App({
     data:'',
     id: '',
     appId:'wx07f1bc4349dac685',
-    // url: 'https://share.ty-gz.com/share-wx',
-    url: 'http://192.168.123.61:9000'
+    url: 'https://share.ty-gz.com/share-wx',
+    // url: 'http://192.168.123.61:9000'
   }
 })
